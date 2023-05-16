07e671018df65aa1e60401d055aeecd72233c1f7
koku <koku@>
2023-05-16 01:17:14 -0500

    Add project files.

---
+++ LibraryManagement/Controllers/EmployeeTablesController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Database_Layer;
+
+namespace LibraryManagement.Controllers
+{
+    public class EmployeeTablesController : Controller
+    {
+        private OnlineLabEntitiesDbEntities db = new OnlineLabEntitiesDbEntities();
+
+        // GET: EmployeeTables
+        public ActionResult Index()
+        {
+            var employeeTables = db.EmployeeTables.Include(e => e.DepartmentTable).Include(e => e.DesignationTable).Include(e => e.DesignationTable1);
+            return View(employeeTables.ToList());
+        }
+
+        // GET: EmployeeTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmployeeTable employeeTable = db.EmployeeTables.Find(id);
+            if (employeeTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeeTable);
+        }
+
+        // GET: EmployeeTables/Create
+        public ActionResult Create()
+        {
+            ViewBag.DepartmentID = new SelectList(db.DepartmentTables, "DepartmentID", "DepartmentName");
+            ViewBag.DesignationID = new SelectList(db.DesignationTables, "DesignationID", "Name");
+            ViewBag.DepartmentID = new SelectList(db.DesignationTables, "DesignationID", "Name");
+            return View();
+        }
+
+        // POST: EmployeeTables/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EmployeeTable employeeTable)
+        {
+            int userid = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            employeeTable.UserID = userid;
+            if (ModelState.IsValid)
+            {
+                db.EmployeeTables.Add(employeeTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DepartmentID = new SelectList(db.DepartmentTables, "DepartmentID", "DepartmentName", employeeTable.DepartmentID);
+            ViewBag.DesignationID = new SelectList(db.DesignationTables, "DesignationID", "Name", employeeTable.DesignationID);
+            ViewBag.DepartmentID = new SelectList(db.DesignationTables, "DesignationID", "Name", employeeTable.DepartmentID);
+            return View(employeeTable);
+        }
+
+        // GET: EmployeeTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmployeeTable employeeTable = db.EmployeeTables.Find(id);
+            if (employeeTable == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DepartmentID = new SelectList(db.DepartmentTables, "DepartmentID", "DepartmentName", employeeTable.DepartmentID);
+            ViewBag.DesignationID = new SelectList(db.DesignationTables, "DesignationID", "Name", employeeTable.DesignationID);
+            ViewBag.DepartmentID = new SelectList(db.DesignationTables, "DesignationID", "Name", employeeTable.DepartmentID);
+            return View(employeeTable);
+        }
+
+        // POST: EmployeeTables/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(EmployeeTable employeeTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employeeTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartmentID = new SelectList(db.DepartmentTables, "DepartmentID", "DepartmentName", employeeTable.DepartmentID);
+            ViewBag.DesignationID = new SelectList(db.DesignationTables, "DesignationID", "Name", employeeTable.DesignationID);
+            ViewBag.DepartmentID = new SelectList(db.DesignationTables, "DesignationID", "Name", employeeTable.DepartmentID);
+            return View(employeeTable);
+        }
+
+        // GET: EmployeeTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmployeeTable employeeTable = db.EmployeeTables.Find(id);
+            if (employeeTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeeTable);
+        }
+
+        // POST: EmployeeTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EmployeeTable employeeTable = db.EmployeeTables.Find(id);
+            db.EmployeeTables.Remove(employeeTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LibraryManagement/Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using Database_Layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private OnlineLabEntitiesDbEntities db = new OnlineLabEntitiesDbEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginUser(string username, string password)
+        {
+            try {
+                if(username != null && password != null)
+                {
+                    var findUser = db.UserTables.Where(u => u.UserName == username && u.Password == password && u.IsActive == true).ToList();
+                    if (findUser.Count() == 1)
+                    {
+                        Session["UserID"] = findUser[0].UserID;
+                        Session["UserTypeID"] = findUser[0].UserTypeID;
+                        Session["UserName"] = findUser[0].UserName;
+                        Session["Password"] = findUser[0].Password;
+                        Session["EmployeeID"] = findUser[0].EmployeeID;
+
+                        string url = string.Empty;
+                        if (findUser[0].UserTypeID == 2)
+                        {
+                            return RedirectToAction("About");
+
+                        }
+                        else if (findUser[0].UserTypeID == 3)
+                        {
+                            return RedirectToAction("About");
+
+                        }
+                        else if (findUser[0].UserTypeID == 4)
+                        {
+                            return RedirectToAction("About");
+                        }
+                        else if (findUser[0].UserTypeID == 1)
+                        {
+                            url = "About";
+                        }
+                        else
+                        {
+                            url = "About";
+                        }
+
+                        return RedirectToAction(url);
+                    }
+                    else
+                    {
+                        Session["UserID"] = string.Empty;
+                        Session["UserTypeID"] = string.Empty;
+                        Session["UserName"] = string.Empty;
+                        Session["Password"] = string.Empty;
+                        Session["EmployeeID"] = string.Empty;
+                        ViewBag.message = "User Name and Password is incorrect!";
+                    }
+                }
+                else
+                {
+                    Session["UserID"] = string.Empty;
+                    Session["UserTypeID"] = string.Empty;
+                    Session["UserName"] = string.Empty;
+                    Session["Password"] = string.Empty;
+                    Session["EmployeeID"] = string.Empty;
+                    ViewBag.message = "Some unexpected issus is occure please try again!";
+                }
+            } catch (Exception ex)
+            {
+                Session["UserID"] = string.Empty;
+                Session["UserTypeID"] = string.Empty;
+                Session["UserName"] = string.Empty;
+                Session["Password"] = string.Empty;
+                Session["EmployeeID"] = string.Empty;
+                ViewBag.message = "Some unexpected issue is occure please try again !";
+            }
+            return View("Login");
+        }
+        public ActionResult Logout()
+        {
+            Session["UserID"] = string.Empty;
+            Session["UserTypeID"] = string.Empty;
+            Session["Password"] = string.Empty;
+            Session["EmployeeID"] = string.Empty;
+            return RedirectToAction("Login");
+        }
+    }
+}
